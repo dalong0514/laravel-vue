6a0b06a6a3f34d5de02fe04679bd51e47d41efb8
dalong <dalong0514@126.com>
2020-06-16 10:42:19 +0800

    自定义的测试文件

---
+++ spec/dalongTest/testSpec.js
@@ -0,0 +1,14 @@
+function convertCurrency(amount,rateOfConversion) {
+    var toCurrencyAmount = 0; 
+    // conversion 
+    toCurrencyAmount = rateOfConversion * amount; 
+    // rounding off 
+    toCurrencyAmount = parseFloat(toCurrencyAmount).toFixed(2); 
+    return toCurrencyAmount;
+}
+
+describe('Convert Currency', function() { 
+    it('100 INR should be equal to $ 1.59', function() { 
+        expect(convertCurrency(100, 1/63)).toEqual('1.59'); 
+    }); 
+});
