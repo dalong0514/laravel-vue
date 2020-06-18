7e25c5159f23931f2c3752ecd2a2e694d34a9f14
dalong <dalong0514@126.com>
2020-06-19 00:02:30 +0800

    3.4.2 Taking the Hint: Object Types

--- resources/views/codeTest.php
+++ resources/views/codeTest.php
@@ -1,5 +1,5 @@
 <?php
-
+// declare(strict_types=1);  // 显式声明类型检查为严格模式
 // use APP\ShopProduct;
 
 class ShopProduct {
@@ -8,10 +8,31 @@ class ShopProduct {
     public $productFirstName = 'first name';
     public $price = 0;
 
+    public function __construct(
+        string $title,
+        string $firstName,
+        string $mainName,
+        float $price) {
+        $this->title = $title;
+        $this->productMainName = $mainName;
+        $this->productFirstName = $firstName;
+        $this->price = $price;
+    }
+
     public function getProducer() {
         return $this->productFirstName . ' ' . $this->productMainName;
     }
 }
 
-$product = new ShopProduct();
-dd($product->title);
+class ShopProductWriter {
+    public function write(ShopProduct $shopProduct) {
+        $str = $shopProduct->title . ': '
+            . $shopProduct->getProducer()
+            . '(' . $shopProduct->price . ")\n";
+        print $str;
+    }
+}
+
+$product = new ShopProduct('My Antonia', 'Willa', 'Cather', '6.6');
+$write = new ShopProductWriter();
+$write->write($product);
