d4cc0d6579da32beddd96667eba2a63ddd3eb8e9
dalong <dalong0514@126.com>
2020-06-19 09:40:42 +0800

    #### 3.5.2 Working with Inheritance

--- resources/views/codeTest.php
+++ resources/views/codeTest.php
@@ -3,24 +3,62 @@
 // use APP\ShopProduct;
 
 class ShopProduct {
+    public $numPages;
+    public $playLength;
     public $title = 'default product';
-    public $productMainName = 'main name';
-    public $productFirstName = 'first name';
+    public $producerMainName = 'main name';
+    public $producerFirstName = 'first name';
     public $price = 0;
 
     public function __construct(
         string $title,
         string $firstName,
         string $mainName,
-        float $price) {
+        float $price,
+        int $numPages = 0,
+        int $playLength = 0) {
         $this->title = $title;
-        $this->productMainName = $mainName;
-        $this->productFirstName = $firstName;
+        $this->producerMainName = $mainName;
+        $this->producerFirstName = $firstName;
         $this->price = $price;
+        $this->numPages = $numPages;
+        $this->playLength = $playLength;
     }
 
     public function getProducer() {
-        return $this->productFirstName . ' ' . $this->productMainName;
+        return $this->producerFirstName . ' ' . $this->producerMainName;
+    }
+
+    public function getSummaryLine() {
+        $base = "{$this->title} ({$this->produceMainName}, ";
+        $base .= "{$this->producerFirstName} )";
+        return $base;
+    }
+}
+
+class CdProduct extends ShopProduct {
+    public function getPlayLength() {
+        return $this->playLength;
+    }
+
+    public function getSummaryLine() {
+        $base = "{$this->title} ({$this->produceMainName}, ";
+        $base .= "{$this->producerFirstName} )";
+        $base .= ": playing time - {$this->playLength}";
+        return $base;
+    }
+}
+
+class BookProduct extends ShopProduct {
+    function getNumberOfPages() {
+        return $this->numPages;
+    }
+
+    public function getSummaryLine() {
+        $base = "{$this->title} ({$this->produceMainName}, ";
+        $base .= "{$this->producerFirstName} )";
+        $base .= ": page count - {$this->numPages}";
+        return $base;
     }
 }
 
@@ -33,6 +71,16 @@ class ShopProductWriter {
     }
 }
 
-$product = new ShopProduct('My Antonia', 'Willa', 'Cather', '6.6');
-$write = new ShopProductWriter();
-$write->write($product);
+// $product = new ShopProduct('My Antonia', 'Willa', 'Cather', '6.6');
+// $write = new ShopProductWriter();
+// $write->write($product);
+
+$product2 = new CdProduct(
+    'Exile on Coldharbour Lane',
+    'The',
+    'Alabama 3',
+    10.99,
+    0,
+    60.33
+);
+print "artist: {$product2->getProducer()}";
